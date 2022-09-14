fd2ad73ad85602c82e21fd071cc8592d12fa6e7f
Rob Letts <108027101+rob-letts@users.noreply.github.com>
2022-09-14 21:35:42 +0100

    setup in-source testing

--- src/scripts/main.ts
+++ src/scripts/main.ts
@@ -1,1 +1,12 @@
 import '@/styles/global.css';
+
+export function doubleNumber (param: number) {
+  return param * 2;
+}
+
+if (import.meta.vitest) {
+  const { it, expect } = import.meta.vitest;
+  it(`doubles input number`, () => {
+    expect(doubleNumber(1)).toBe(2);
+  });
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 import presetEnv from 'postcss-preset-env';
 import autoprefixer from 'autoprefixer';
 import { resolve } from 'path';
@@ -6,5 +6,7 @@ import { resolve } from 'path';
 export default defineConfig({
   server: { port: 3000 },
   css: { postcss: { plugins: [presetEnv({ stage: 1 }), autoprefixer] }},
-  resolve: { alias: { "@": resolve(__dirname, `src`) }}
+  resolve: { alias: { "@": resolve(__dirname, `src`) }},
+  test: { includeSource: [`src/**/*.{js,ts}`] },
+  define: { 'import.meta.vitest': `undefined` }
 });
