0cd7938689d63e0af188d81442206fac73c7fa4e
Rob Letts <108027101+rob-letts@users.noreply.github.com>
2022-09-18 08:14:27 +0100

    setup optional vue dependency

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from 'vitest/config';
 import presetEnv from 'postcss-preset-env';
 import autoprefixer from 'autoprefixer';
 import { resolve } from 'path';
+// import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   server: { port: 3000 },
   css: { postcss: { plugins: [presetEnv({ stage: 1 }), autoprefixer] }},
   resolve: { alias: { "@": resolve(__dirname, `src`) }},
   test: { includeSource: [`src/**/*.{js,ts}`] },
+  // plugins: [vue()],
   define: { 'import.meta.vitest': `undefined` }
 });
