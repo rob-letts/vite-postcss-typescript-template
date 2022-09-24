748dfd4d87ee09b1bff8706ea9b5f5562b7f14aa
Rob Letts <108027101+rob-letts@users.noreply.github.com>
2022-09-24 12:07:25 +0100

    restored defaults and removed optional dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,10 @@
-import { defineConfig } from 'vitest/config';
+import { defineConfig } from 'vite';
 import presetEnv from 'postcss-preset-env';
 import autoprefixer from 'autoprefixer';
 import { resolve } from 'path';
-// import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   server: { port: 3000 },
   css: { postcss: { plugins: [presetEnv({ stage: 1 }), autoprefixer] }},
-  resolve: { alias: { "@": resolve(__dirname, `src`) }},
-  test: { includeSource: [`src/**/*.{js,ts}`] },
-  // plugins: [vue()],
-  define: { 'import.meta.vitest': `undefined` }
+  resolve: { alias: { "@": resolve(__dirname, `src`) }}
 });
